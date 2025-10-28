86582ce32f3a07bd980af87e708a36618fcfb785
Kiril Savchev <k.savchev@gmail.com>
2025-10-28 19:33:24 +0200

    Add server to execute remote procedures

--- src/RemoteProcedureInterface.php
+++ src/RemoteProcedureInterface.php
@@ -2,7 +2,17 @@
 
 namespace Alcedo\JsonRpc\Server;
 
+use Alcedo\JsonRpc\Server\DTO\Response;
+
+/**
+ * Defines an interface for remote procedure calls.
+ */
 interface RemoteProcedureInterface
 {
-
+    /**
+     * Executes the call and returns the response.
+     *
+     * @return Response The response object resulting from the call.
+     */
+    public function call(): Response;
 }
