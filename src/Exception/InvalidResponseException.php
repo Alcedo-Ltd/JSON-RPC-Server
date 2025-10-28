18d22d38f87e9022919c1cbc1e6b735c53db3dca
Kiril Savchev <k.savchev@gmail.com>
2025-10-28 15:11:06 +0200

    Add the error object

--- src/Exception/InvalidResponseException.php
+++ src/Exception/InvalidResponseException.php
@@ -2,6 +2,11 @@
 
 namespace Alcedo\JsonRpc\Server\Exception;
 
+/**
+ * Represents an exception that is thrown when an invalid response is encountered.
+ * This exception is typically used to indicate that a response does not meet
+ * expected or required criteria.
+ */
 class InvalidResponseException extends \Exception
 {
     // ...
